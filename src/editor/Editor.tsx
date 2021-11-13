f7fe896df7d6ae8c13951cd326032180bd7a8403
Florian Wiech <florian.wiech@gmail.com>
2021-11-13 17:06:59 +0100

    feat: add custom editor setup

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -1,8 +1,31 @@
-import { basicSetup, EditorView } from "@codemirror/basic-setup";
-import { indentWithTab } from "@codemirror/commands";
+import { defaultKeymap, indentWithTab } from "@codemirror/commands";
 import { EditorState } from "@codemirror/state";
-import { keymap } from "@codemirror/view";
+import {
+  drawSelection,
+  EditorView,
+  highlightActiveLine,
+  highlightSpecialChars,
+  keymap,
+} from "@codemirror/view";
 import { createRef, useEffect, useRef } from "react";
+import { javascript } from "@codemirror/lang-javascript";
+import { highlightActiveLineGutter, lineNumbers } from "@codemirror/gutter";
+import { closeBrackets, closeBracketsKeymap } from "@codemirror/closebrackets";
+import { lintKeymap } from "@codemirror/lint";
+import {
+  highlightSelectionMatches,
+  searchConfig,
+  searchKeymap,
+} from "@codemirror/search";
+import { foldGutter, foldKeymap } from "@codemirror/fold";
+import { commentKeymap } from "@codemirror/comment";
+import { history, historyKeymap } from "@codemirror/history";
+import { indentOnInput } from "@codemirror/language";
+import { defaultHighlightStyle } from "@codemirror/highlight";
+import { autocompletion, completionKeymap } from "@codemirror/autocomplete";
+import { rectangularSelection } from "@codemirror/rectangular-selection";
+import { bracketMatching } from "@codemirror/matchbrackets";
+import { initialContent } from "./initialContent";
 
 export const Editor = () => {
   const ref = createRef<HTMLDivElement>();
@@ -12,12 +35,42 @@ export const Editor = () => {
     if (!ref.current) return;
 
     const startState = EditorState.create({
-      extensions: [basicSetup, keymap.of([indentWithTab])],
+      doc: initialContent,
+      extensions: [
+        lineNumbers(),
+        highlightActiveLineGutter(),
+        highlightSpecialChars(),
+        history(),
+        foldGutter(),
+        drawSelection(),
+        EditorState.allowMultipleSelections.of(true),
+        indentOnInput(),
+        defaultHighlightStyle.fallback,
+        bracketMatching(),
+        closeBrackets(),
+        autocompletion(),
+        rectangularSelection(),
+        highlightActiveLine(),
+        highlightSelectionMatches(),
+        searchConfig({ top: true }),
+        keymap.of([
+          ...closeBracketsKeymap,
+          ...defaultKeymap,
+          ...searchKeymap,
+          ...historyKeymap,
+          ...foldKeymap,
+          ...commentKeymap,
+          ...completionKeymap,
+          ...lintKeymap,
+          indentWithTab,
+        ]),
+        javascript(),
+      ],
     });
 
     viewRef.current = new EditorView({
       state: startState,
-      parent: document.body,
+      parent: ref.current,
     });
 
     return () => viewRef.current?.destroy();
---
+++ src/editor/initialContent.ts
@@ -0,0 +1,18 @@
+export const initialContent = `
+
+
+Here is some content
+
+{
+  "foo": "bar"
+}
+
+export function name(params) {
+  console.log("test");
+}
+
+export function name2(params) {
+  console.log("test");
+}
+
+`;
