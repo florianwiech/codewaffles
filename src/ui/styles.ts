885e1227217444a5a76600ab8dc87fa0d5adc5ec
Florian Wiech <florian.wiech@gmail.com>
2021-11-13 13:05:52 +0100

    feat: use typography from @primer/primitives

--- src/ui/styles.ts
+++ src/ui/styles.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import typography from "@primer/primitives/dist/ts/typography/normal";
 
 export const GlobalStyle = createGlobalStyle`
   html, body {
@@ -9,7 +10,9 @@ export const GlobalStyle = createGlobalStyle`
     background-color: ${({ theme }) => theme.canvas.default};
 
     font-family: "SF Pro Display", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Open Sans", "Helvetica Neue", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-    font-size: 14px;
-    line-height: 21px;
+    font-size: ${typography.fontSize[1]};
+    line-height: ${typography.lineHeight.default};
+
+    overflow: hidden;
   }
 `;
