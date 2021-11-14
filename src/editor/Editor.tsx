c152580f9b1afbd8b0d2ea1f99aea2be0bfb8dec
Florian Wiech <florian.wiech@gmail.com>
2021-11-14 16:50:13 +0100

    refactor: extract basic editor setup

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -1,39 +1,14 @@
-import { defaultKeymap, indentWithTab } from "@codemirror/commands";
 import { EditorState } from "@codemirror/state";
-import {
-  drawSelection,
-  EditorView,
-  highlightActiveLine,
-  highlightSpecialChars,
-  keymap,
-} from "@codemirror/view";
-import { createRef, useEffect, useRef } from "react";
+import { EditorView } from "@codemirror/view";
+import { useEffect, useRef } from "react";
 import { javascript } from "@codemirror/lang-javascript";
-import { highlightActiveLineGutter, lineNumbers } from "@codemirror/gutter";
-import { closeBrackets, closeBracketsKeymap } from "@codemirror/closebrackets";
-import { lintKeymap } from "@codemirror/lint";
-import {
-  highlightSelectionMatches,
-  searchConfig,
-  searchKeymap,
-} from "@codemirror/search";
-import { foldGutter, foldKeymap } from "@codemirror/fold";
-import { commentKeymap } from "@codemirror/comment";
-import { history, historyKeymap } from "@codemirror/history";
-import { indentOnInput } from "@codemirror/language";
-import { defaultHighlightStyle } from "@codemirror/highlight";
-import { autocompletion, completionKeymap } from "@codemirror/autocomplete";
-import { rectangularSelection } from "@codemirror/rectangular-selection";
-import { bracketMatching } from "@codemirror/matchbrackets";
-import { useLayout } from "../ui";
-import { AppearanceState } from "../ui/appearance";
 import { StyledEditor } from "./Editor.style";
 import { initialContent } from "./initialContent";
-import { primerDark, primerLight } from "./primerTheme";
+import { primerDark } from "./primerTheme";
+import { basics } from "./setup/basics";
 
 export const Editor = () => {
-  const theme = useLayout();
-  const ref = createRef<HTMLDivElement>();
+  const ref = useRef<HTMLDivElement>(null);
   const viewRef = useRef<EditorView>();
 
   useEffect(() => {
@@ -41,46 +16,15 @@ export const Editor = () => {
 
     const startState = EditorState.create({
       doc: initialContent,
-      extensions: [
-        lineNumbers(),
-        highlightActiveLineGutter(),
-        highlightSpecialChars(),
-        history(),
-        foldGutter(),
-        drawSelection(),
-        EditorState.allowMultipleSelections.of(true),
-        indentOnInput(),
-        defaultHighlightStyle.fallback,
-        bracketMatching(),
-        closeBrackets(),
-        autocompletion(),
-        rectangularSelection(),
-        highlightActiveLine(),
-        highlightSelectionMatches(),
-        searchConfig({ top: true }),
-        keymap.of([
-          ...closeBracketsKeymap,
-          ...defaultKeymap,
-          ...searchKeymap,
-          ...historyKeymap,
-          ...foldKeymap,
-          ...commentKeymap,
-          ...completionKeymap,
-          ...lintKeymap,
-          indentWithTab,
-        ]),
-        javascript(),
-        theme === AppearanceState.LIGHT ? primerLight : primerDark,
-      ],
+      extensions: [basics, javascript(), primerDark],
     });
 
     viewRef.current = new EditorView({
       state: startState,
       parent: ref.current,
     });
-
     return () => viewRef.current?.destroy();
-  }, [ref, theme]);
+  }, []);
 
   return <StyledEditor ref={ref} />;
 };
---
+++ src/editor/setup/basics.ts
@@ -0,0 +1,54 @@
+import { highlightActiveLineGutter, lineNumbers } from "@codemirror/gutter";
+import {
+  drawSelection,
+  highlightActiveLine,
+  highlightSpecialChars,
+  keymap,
+} from "@codemirror/view";
+import { history, historyKeymap } from "@codemirror/history";
+import { foldGutter, foldKeymap } from "@codemirror/fold";
+import { EditorState } from "@codemirror/state";
+import { indentOnInput } from "@codemirror/language";
+import { defaultHighlightStyle } from "@codemirror/highlight";
+import { bracketMatching } from "@codemirror/matchbrackets";
+import { closeBrackets, closeBracketsKeymap } from "@codemirror/closebrackets";
+import { autocompletion, completionKeymap } from "@codemirror/autocomplete";
+import { rectangularSelection } from "@codemirror/rectangular-selection";
+import {
+  highlightSelectionMatches,
+  searchConfig,
+  searchKeymap,
+} from "@codemirror/search";
+import { defaultKeymap, indentWithTab } from "@codemirror/commands";
+import { commentKeymap } from "@codemirror/comment";
+import { lintKeymap } from "@codemirror/lint";
+
+export const basics = [
+  lineNumbers(),
+  highlightActiveLineGutter(),
+  highlightSpecialChars(),
+  history(),
+  foldGutter(),
+  drawSelection(),
+  EditorState.allowMultipleSelections.of(true),
+  indentOnInput(),
+  defaultHighlightStyle.fallback,
+  bracketMatching(),
+  closeBrackets(),
+  autocompletion(),
+  rectangularSelection(),
+  highlightActiveLine(),
+  highlightSelectionMatches(),
+  searchConfig({ top: true }),
+  keymap.of([
+    ...closeBracketsKeymap,
+    ...defaultKeymap,
+    ...searchKeymap,
+    ...historyKeymap,
+    ...foldKeymap,
+    ...commentKeymap,
+    ...completionKeymap,
+    ...lintKeymap,
+    indentWithTab,
+  ]),
+];
