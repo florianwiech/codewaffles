ca3d1700fab532d5f7f75ea7540a08420406ed1f
Florian Wiech <florian.wiech@gmail.com>
2021-11-19 10:03:40 +0100

    feat: use bigger font size in editor

--- src/editor/Editor.style.ts
+++ src/editor/Editor.style.ts
@@ -62,9 +62,10 @@ export const StyledEditor = styled.div`
 
     .cm-scroller {
       height: 100%;
-      font-size: ${typography.fontSize[0]};
+      font-size: ${typography.fontSize[1]};
       font-family: "SFMono Regular", Menlo, Monaco, Consolas, "Liberation Mono",
         "Courier New", monospace;
+      line-height: ${typography.lineHeight.default};
     }
 
     .cm-cursor {
