c88f26d8ef3fe93606f4c7468157a18520422b45
Florian Wiech <florian.wiech@gmail.com>
2021-11-19 12:08:34 +0100

    feat: extend search escape key behaviour
    Clicking escape on an active search with displayed results only resets the search but does not close it. An invalid search or empty search gets closed on escape key

--- src/spotlight-search/Spotlight.tsx
+++ src/spotlight-search/Spotlight.tsx
@@ -1,4 +1,4 @@
-import { FC, KeyboardEventHandler, useState } from "react";
+import { ChangeEventHandler, FC, KeyboardEventHandler, useState } from "react";
 import debounce from "lodash.debounce";
 import Fuse from "fuse.js";
 import { useKeyPress } from "../utils/useKeyPress";
@@ -14,7 +14,10 @@ export const Spotlight: FC<Props> = ({ scripts }) => {
   const fuse = new Fuse(scripts, { keys: ["label"] });
 
   const [visible, setVisible] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
   const [hits, setHits] = useState<Fuse.FuseResult<TransformDefinition>[]>([]);
+
+  const activeSearch = hits.length > 0;
 
   const handleKeyPress = () => setVisible(!visible);
   useKeyPress("k", handleKeyPress);
@@ -22,8 +25,14 @@ export const Spotlight: FC<Props> = ({ scripts }) => {
   // todo style spotlight (responsiveness)
   // todo keyboard navigation between results
 
-  const clearSearch = () => {
+  const closeSearch = () => {
     setVisible(false);
+    resetSearch();
+  };
+
+  const resetSearch = () => {
+    setSearchInput("");
+    setHits([]);
   };
 
   const handleKeyboardShortcuts: KeyboardEventHandler<HTMLInputElement> = (
@@ -32,15 +41,24 @@ export const Spotlight: FC<Props> = ({ scripts }) => {
     switch (event.key) {
       // todo cases arrow keys for selection
       case "Escape":
-        clearSearch();
+        if (activeSearch) {
+          resetSearch();
+        } else {
+          closeSearch();
+        }
         event.preventDefault();
         break;
     }
   };
 
-  const handleSearch = debounce((term: string) => {
+  const performSearch = debounce((term: string) => {
     setHits(fuse.search(term));
   }, 500);
+
+  const handleSearch: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setSearchInput(event.target.value);
+    performSearch(event.target.value);
+  };
 
   if (!visible) {
     return null;
@@ -48,12 +66,13 @@ export const Spotlight: FC<Props> = ({ scripts }) => {
 
   return (
     <>
-      <StyledBackdrop onClick={clearSearch} />
+      <StyledBackdrop onClick={closeSearch} />
       <StyledSpotlight>
         <StyledInput
           type="text"
           placeholder="Search command..."
-          onChange={(event) => handleSearch(event.target.value)}
+          value={searchInput}
+          onChange={handleSearch}
           onKeyDown={handleKeyboardShortcuts}
           autoFocus
         />
