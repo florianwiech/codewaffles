39c460d21e988113bce7012fcfcf94735dd0f8d6
Florian Wiech <florian.wiech@gmail.com>
2021-11-20 13:59:48 +0100

    chore: add user agent test mock

---
+++ src/shared/testing/mockUserAgent.ts
@@ -0,0 +1,22 @@
+const origin = global.navigator.userAgent;
+const cleared = Symbol("clear");
+let fakeUserAgent: any = null;
+
+Object.defineProperty(global.navigator, "userAgent", {
+  get() {
+    return fakeUserAgent === cleared ? origin : fakeUserAgent || "";
+  },
+});
+
+export const clear = () => {
+  fakeUserAgent = cleared;
+};
+
+export const mockUserAgent = (agent: string) => {
+  fakeUserAgent = agent;
+};
+
+export const userAgentWindowsMock =
+  "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
+export const userAgentMacOSMock =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0";
