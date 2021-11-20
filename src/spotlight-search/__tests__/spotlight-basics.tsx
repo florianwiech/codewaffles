f104bc8a436c75aa8d060b8b12843bdfe35aa923
Florian Wiech <florian.wiech@gmail.com>
2021-11-20 09:16:58 +0100

    refactor: split spotlight tests into multiple files

---
+++ src/spotlight-search/__tests__/spotlight-basics.tsx
@@ -0,0 +1,53 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import Fuse from "fuse.js";
+import { Spotlight, SPOTLIGHT_LABEL } from "../Spotlight";
+import { TransformList } from "../../transforms";
+import { Layout } from "../../ui";
+
+export const scriptsMock: TransformList = [
+  { key: "one", label: "one", version: 1, handler: () => "" },
+  { key: "two", label: "two", version: 1, handler: () => "" },
+  { key: "three", label: "three", version: 1, handler: () => "" },
+];
+
+export const setupOpenSpotlight = async () => {
+  const utils = render(
+    <Layout>
+      <Spotlight scripts={scriptsMock} />
+    </Layout>
+  );
+
+  fireEvent.keyDown(utils.baseElement, {
+    key: "k",
+    ctrlKey: true,
+    metaKey: true,
+  });
+
+  await waitFor(() => {
+    expect(screen.getByPlaceholderText(SPOTLIGHT_LABEL)).toBeInTheDocument();
+  });
+
+  const inputElement = screen.getByPlaceholderText(SPOTLIGHT_LABEL);
+  return {
+    inputElement,
+    ...utils,
+  };
+};
+
+const defaultFuseSearchMock = [
+  { item: scriptsMock[0] },
+  { item: scriptsMock[1] },
+] as Fuse.FuseResult<unknown>[];
+
+export const createFuseSearchSpy = (returnValue = defaultFuseSearchMock) =>
+  jest.spyOn(Fuse.prototype, "search").mockReturnValue(returnValue);
+
+it("hidden by default", () => {
+  render(<Spotlight scripts={scriptsMock} />);
+
+  expect(
+    screen.queryByPlaceholderText(SPOTLIGHT_LABEL)
+  ).not.toBeInTheDocument();
+});
+
+it("open via keyboard shortcut", async () => await setupOpenSpotlight());
