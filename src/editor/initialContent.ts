d56c3275bf492c3f74c67159ad05e846783dafa5
Florian Wiech <florian.wiech@gmail.com>
2021-11-22 21:02:06 +0100

    chore: update initial test content

--- src/editor/initialContent.ts
+++ src/editor/initialContent.ts
@@ -15,4 +15,6 @@ export function name2(params) {
   console.log("test");
 }
 
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+
 `;
