56c12b7b436358ec38057c3e81fdf215e2f8e9c8
Florian Wiech <florian.wiech@gmail.com>
2021-12-04 11:31:55 +0100

    feat: add useObservable hook

---
+++ src/shared/hooks/useObservable.spec.ts
@@ -0,0 +1,48 @@
+import { act, cleanup, renderHook } from "@testing-library/react-hooks";
+import { BehaviorSubject, Subject, Subscription } from "rxjs";
+import { useObservable } from "./useObservable";
+
+describe("useObservable", () => {
+  it("should have undefined initial state", () => {
+    const test$ = new Subject<string>();
+
+    const { result } = renderHook(() => useObservable(test$));
+
+    expect(result.current).toBe(undefined);
+  });
+
+  it("should have initial state with data", () => {
+    const test$ = new BehaviorSubject<string>("initial");
+
+    const { result } = renderHook(() => useObservable(test$));
+
+    expect(result.current).toBe("initial");
+  });
+
+  it("should set state on  new emits", async () => {
+    const test$ = new Subject<string>();
+
+    const { result } = renderHook(() => useObservable(test$));
+
+    act(() => {
+      test$.next("next");
+    });
+
+    expect(result.current).toBe("next");
+  });
+
+  it("should cleanup subscription", () => {
+    const test$ = new Subject<string>();
+
+    const subscription = new Subscription();
+    const unsubscribeSpy = jest.spyOn(subscription, "unsubscribe");
+
+    jest.spyOn(test$, "subscribe").mockReturnValue(subscription);
+
+    renderHook(() => useObservable(test$));
+
+    cleanup();
+
+    expect(unsubscribeSpy).toHaveBeenCalled();
+  });
+});
