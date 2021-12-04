6f0c87cbcb7dde39ca68cff2d8599e9fa3ec44af
Florian Wiech <florian.wiech@gmail.com>
2021-12-04 19:53:27 +0100

    refactor: rename view to editor inside streams

--- src/streams/index.ts
+++ src/streams/index.ts
@@ -1,3 +1,3 @@
 export * from "./command";
-export * from "./view";
+export * from "./editor";
 export * from "./types";
--- src/streams/types/index.ts
+++ src/streams/types/index.ts
@@ -1,2 +1,2 @@
 export * from "./command";
-export * from "./view";
+export * from "./editor";
