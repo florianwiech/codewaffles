49dd8c8db575f285d59c93ad340875a72edaf646
Florian Wiech <florian.wiech@gmail.com>
2021-12-08 17:54:47 +0100

    refactor: replace combineLatestObject with combineLatest option & remove rxjs-etc dependency

--- src/streams/editor.ts
+++ src/streams/editor.ts
@@ -1,7 +1,7 @@
-import { BehaviorSubject, merge } from "rxjs";
+import { BehaviorSubject, combineLatest, merge } from "rxjs";
 import { filter, tap } from "rxjs/operators";
 import { EditorView } from "@codemirror/view";
-import { combineLatestObject } from "rxjs-etc";
+import { tag } from "rxjs-spy/cjs/operators";
 import { isEditorView } from "./types";
 import { closeSearch$, performTransform$ } from "./command";
 import { transformContent, transformRanges } from "./operators";
@@ -10,21 +10,22 @@ export const editor$ = new BehaviorSubject<EditorView | null>(null);
 
 export const view$ = editor$.pipe(filter(isEditorView));
 
-export const editorTransform$ = combineLatestObject({
-  command: performTransform$,
-  view: view$,
-});
+export const editorTransform$ = combineLatest(
+  [performTransform$, view$],
+  (command, view) => ({ command, view }),
+);
 
 export const transforms$ = merge(
   editorTransform$.pipe(transformContent()),
   editorTransform$.pipe(transformRanges()),
 ).pipe(tap(({ view, tr }) => view.dispatch(tr)));
 
-export const closeSearchExtended$ = combineLatestObject({
-  command: closeSearch$,
-  view: view$,
-});
+export const closeSearchExtended$ = combineLatest(
+  [closeSearch$, view$],
+  (command, view) => ({ command, view }),
+);
 
 export const editorChanges$ = merge(closeSearchExtended$, transforms$).pipe(
   tap(({ view }) => view.focus()),
+  tag("editor-changes"),
 );
