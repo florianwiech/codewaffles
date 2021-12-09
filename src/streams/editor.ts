8368cb221f5968447199de5ef2ab8039111d7e96
Florian Wiech <florian.wiech@gmail.com>
2021-12-09 21:53:18 +0100

    refactor: editor changes observable via getEditorChanges function

--- src/streams/editor.ts
+++ src/streams/editor.ts
@@ -1,9 +1,14 @@
-import { BehaviorSubject, combineLatest, merge } from "rxjs";
+import { BehaviorSubject, combineLatest, merge, Observable } from "rxjs";
 import { filter, tap } from "rxjs/operators";
 import { EditorView } from "@codemirror/view";
 import { tag } from "rxjs-spy/cjs/operators";
-import { isEditorView } from "./types";
-import { closeSearch$, performTransform$ } from "./command";
+import {
+  Command,
+  CommandTypes,
+  isEditorView,
+  isPerformTransformCommand,
+} from "./types";
+import { closeSearch$, command$, performTransform$ } from "./command";
 import { transformContent, transformRanges } from "./operators";
 
 export const editor$ = new BehaviorSubject<EditorView | null>(null);
@@ -29,3 +34,31 @@ export const editorChanges$ = merge(closeSearchExtended$, transforms$).pipe(
   tap(({ view }) => view.focus()),
   tag("editor-changes"),
 );
+
+export const getEditorChanges = (source$: Observable<Command>) => {
+  const closeSearch$ = source$.pipe(
+    filter(({ type }) => type === CommandTypes.SEARCH_CLOSED),
+  );
+
+  const performTransform$ = source$.pipe(filter(isPerformTransformCommand));
+
+  const editorTransform$ = combineLatest(
+    [performTransform$, view$],
+    (command, view) => ({ command, view }),
+  );
+
+  const transforms$ = merge(
+    editorTransform$.pipe(transformContent()),
+    editorTransform$.pipe(transformRanges()),
+  ).pipe(tap(({ view, tr }) => view.dispatch(tr)));
+
+  const closeSearchExtended$ = combineLatest(
+    [closeSearch$, view$],
+    (command, view) => ({ command, view }),
+  );
+
+  return merge(closeSearchExtended$, transforms$).pipe(
+    tap(({ view }) => view.focus()),
+    tag("editor-changes"),
+  );
+};
