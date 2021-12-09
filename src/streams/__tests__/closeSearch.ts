b2f3e40745502181107e5affb721c26731666694
Florian Wiech <florian.wiech@gmail.com>
2021-12-09 17:57:14 +0100

    test: use plain jest for closeSearch test (instead of TestScheduler)

--- src/streams/__tests__/closeSearch.ts
+++ src/streams/__tests__/closeSearch.ts
@@ -8,37 +8,62 @@ import { CommandTypes } from "../types";
 // https://stackoverflow.com/questions/61465243/testscheduler-for-subject
 // https://www.youtube.com/watch?v=s9FY-MBW1rc
 describe("close search command", () => {
-  let testScheduler: TestScheduler;
+  // let testScheduler: TestScheduler;
+  //
+  // beforeEach(() => {
+  //   testScheduler = new TestScheduler((actual, expected) =>
+  //     expect(actual).toEqual(expected),
+  //   );
+  // });
+  //
+  // it("should exec close search", () => {
+  //   testScheduler.run((helpers) => {
+  //     const { hot, expectObservable } = helpers;
+  //
+  //     const view = createEditor(document.body);
+  //     editor$.next(view);
+  //
+  //     const command = { type: CommandTypes.SEARCH_CLOSED };
+  //
+  //     const observable = hot("-c", { c: command }).pipe(
+  //       tap((c) => command$.next(c)),
+  //     );
+  //     const obsMarble = "       -c";
+  //     const obsValue = { c: command };
+  //
+  //     const expectedMarble = "-r";
+  //     const expectedValue = { r: { command, view } };
+  //
+  //     expectObservable(observable).toBe(obsMarble, obsValue);
+  //     expectObservable(editorChanges$).toBe(expectedMarble, expectedValue);
+  //
+  //     // CLEANUP
+  //     view.destroy();
+  //   });
+  // });
 
-  beforeEach(() => {
-    testScheduler = new TestScheduler((actual, expected) =>
-      expect(actual).toEqual(expected),
-    );
-  });
+  it("should focus editor", () => {
+    // GIVEN
+    const view = createEditor(document.body);
+    const focusSpy = jest.spyOn(view, "focus");
+    const dispatchSpy = jest.spyOn(view, "dispatch");
 
-  it("should exec close search", () => {
-    testScheduler.run((helpers) => {
-      const { hot, expectObservable } = helpers;
+    const closeSearchCommand = { type: CommandTypes.SEARCH_CLOSED };
 
-      const view = createEditor(document.body);
-      editor$.next(view);
+    // WHEN
+    editor$.next(view);
 
-      const command = { type: CommandTypes.SEARCH_CLOSED };
+    const sub = editorChanges$.subscribe();
 
-      const observable = hot("-c", { c: command }).pipe(
-        tap((c) => command$.next(c)),
-      );
-      const obsMarble = "       -c";
-      const obsValue = { c: command };
+    command$.next(closeSearchCommand);
 
-      const expectedMarble = "-r";
-      const expectedValue = { r: { command, view } };
+    // THEN
+    expect(view.hasFocus).toBeTruthy();
+    expect(focusSpy).toHaveBeenCalled();
+    expect(dispatchSpy).not.toHaveBeenCalled();
 
-      expectObservable(observable).toBe(obsMarble, obsValue);
-      expectObservable(editorChanges$).toBe(expectedMarble, expectedValue);
-
-      // CLEANUP
-      view.destroy();
-    });
+    // CLEANUP
+    sub.unsubscribe();
+    view.destroy();
   });
 });
