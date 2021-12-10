7bb8466ed57237edfca6bd86f2fa9ed3a50018df
Florian Wiech <florian.wiech@gmail.com>
2021-12-10 17:55:02 +0100

    refactor: wrap color scheme listener observable into function
    - extract filter into own function for better testability
    - receive observable via function to early return when apis not available

--- src/ui/utils/browserColorSchemeListener.ts
+++ src/ui/utils/browserColorSchemeListener.ts
@@ -1,4 +1,4 @@
-import { fromEvent, mapTo, withLatestFrom } from "rxjs";
+import { EMPTY, fromEvent, mapTo, withLatestFrom } from "rxjs";
 import { filter, map } from "rxjs/operators";
 import { useEffect } from "react";
 import {
@@ -8,27 +8,35 @@ import {
   theme$,
 } from "../appearance";
 
-export const prefersDarkMode$ = fromEvent<MediaQueryListEvent>(
-  window.matchMedia("(prefers-color-scheme: dark)"),
-  "change",
-).pipe(map(({ matches }) => matches));
+export const shouldUpdateTheme = ([prefersDarkMode, appearance, theme]: [
+  boolean,
+  AppearanceState,
+  Omit<AppearanceState, "system">,
+]) => {
+  if (appearance !== AppearanceState.SYSTEM) return false;
+  return !(prefersDarkMode && theme === AppearanceState.DARK);
+};
 
-export const shouldUpdateTheme$ = prefersDarkMode$.pipe(
-  withLatestFrom(appearance$, theme$),
+export const getColorSchemeChange = () => {
+  if (typeof window === undefined || window?.matchMedia === undefined)
+    return EMPTY;
 
-  filter(([prefersDarkMode, appearance, theme]) => {
-    if (appearance !== AppearanceState.SYSTEM) return false;
-
-    return !(prefersDarkMode && theme === AppearanceState.DARK);
-  }),
-  mapTo(AppearanceState.SYSTEM),
-);
+  return fromEvent<MediaQueryListEvent>(
+    window.matchMedia("(prefers-color-scheme: dark)"),
+    "change",
+  ).pipe(
+    map(({ matches }) => matches),
+    withLatestFrom(appearance$, theme$),
+    filter(shouldUpdateTheme),
+    mapTo(AppearanceState.SYSTEM),
+  );
+};
 
 export const useBrowserAppearanceListener = () => {
   useEffect(() => {
     if (!window.matchMedia) return;
 
-    const sub = shouldUpdateTheme$.subscribe((next) => {
+    const sub = getColorSchemeChange().subscribe((next) => {
       changeAppearance(next, false);
     });
     return () => sub.unsubscribe();
