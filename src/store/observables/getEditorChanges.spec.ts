33686e2f430e637cd965f1d03da2cc286bb83c7e
Florian Wiech <florian.wiech@gmail.com>
2021-12-10 15:33:25 +0100

    test: add getEditorChanges observable tests

---
+++ src/store/observables/getEditorChanges.spec.ts
@@ -0,0 +1,155 @@
+import { BehaviorSubject, first } from "rxjs";
+import { eachValueFrom } from "rxjs-for-await";
+import { EditorSelection } from "@codemirror/state";
+import { createEditor } from "../../editor/setup/createEditor";
+import { Command, CommandTypes } from "../types";
+import { editor$ } from "../subjects";
+import * as Scripts from "../../scripts";
+import { getEditorChanges } from "./getEditorChanges";
+
+describe("getEditorChanges", () => {
+  const cmd$ = new BehaviorSubject<Command>({} as Command);
+
+  const initialContent = `Hello World.`;
+
+  let view = createEditor(document.body, { doc: initialContent });
+  let focusSpy = jest.spyOn(view, "focus");
+  let dispatchSpy = jest.spyOn(view, "dispatch");
+
+  let execScriptSpy = jest.spyOn(Scripts, "execScript");
+  let isAppendableScriptSpy = jest.spyOn(Scripts, "isAppendableScript");
+
+  beforeEach(() => {
+    view = createEditor(document.body, { doc: initialContent });
+    focusSpy = jest.spyOn(view, "focus");
+    dispatchSpy = jest.spyOn(view, "dispatch");
+
+    execScriptSpy.mockRestore();
+    execScriptSpy = jest.spyOn(Scripts, "execScript");
+
+    isAppendableScriptSpy.mockRestore();
+    isAppendableScriptSpy = jest.spyOn(Scripts, "isAppendableScript");
+
+    editor$.next(view);
+  });
+
+  afterEach(() => {
+    editor$.next(null);
+    cmd$.next({} as Command);
+    view.destroy();
+  });
+
+  it("should focus editor when search closed", async () => {
+    const command = { type: CommandTypes.SEARCH_CLOSED };
+    cmd$.next(command);
+
+    const source$ = getEditorChanges(cmd$).pipe(first());
+
+    const result = [];
+    for await (const value of eachValueFrom(source$)) {
+      result.push(value);
+    }
+
+    expect(view.hasFocus).toBeTruthy();
+    expect(view.state.doc.toString()).toBe(initialContent);
+    expect(focusSpy).toHaveBeenCalled();
+    expect(dispatchSpy).not.toHaveBeenCalled();
+    expect(execScriptSpy).not.toHaveBeenCalled();
+  });
+
+  it("should replace content", async () => {
+    execScriptSpy.mockReturnValue("content");
+    isAppendableScriptSpy.mockReturnValue(false);
+
+    const command = {
+      type: CommandTypes.PERFORM_TRANSFORM,
+      key: "create-timestamp-seconds",
+    };
+
+    cmd$.next(command);
+
+    const source$ = getEditorChanges(cmd$).pipe(first());
+
+    const result = [];
+    for await (const value of eachValueFrom(source$)) {
+      result.push(value);
+    }
+
+    expect(view.hasFocus).toBeTruthy();
+    expect(view.state.doc.toString()).toBe("content");
+
+    expect(execScriptSpy).toHaveBeenCalled();
+    expect(isAppendableScriptSpy).toHaveBeenCalled();
+
+    expect(focusSpy).toHaveBeenCalled();
+    expect(dispatchSpy).toHaveBeenCalled();
+  });
+
+  it("should append content", async () => {
+    execScriptSpy.mockReturnValue("content");
+    isAppendableScriptSpy.mockReturnValue(true);
+
+    view.dispatch(
+      view.state.update({
+        selection: EditorSelection.cursor(initialContent.length),
+      }),
+    );
+
+    const command = {
+      type: CommandTypes.PERFORM_TRANSFORM,
+      key: "create-timestamp-seconds",
+    };
+
+    cmd$.next(command);
+
+    const source$ = getEditorChanges(cmd$).pipe(first());
+
+    const result = [];
+    for await (const value of eachValueFrom(source$)) {
+      result.push(value);
+    }
+
+    expect(view.hasFocus).toBeTruthy();
+    expect(view.state.doc.toString()).toBe(`${initialContent}content`);
+
+    expect(execScriptSpy).toHaveBeenCalled();
+    expect(isAppendableScriptSpy).toHaveBeenCalled();
+
+    expect(focusSpy).toHaveBeenCalled();
+    expect(dispatchSpy).toHaveBeenCalled();
+  });
+
+  it("should transform ranges", async () => {
+    execScriptSpy.mockReturnValue("content");
+    isAppendableScriptSpy.mockReturnValue(true);
+
+    view.dispatch(
+      view.state.update({
+        selection: EditorSelection.range(0, 11),
+      }),
+    );
+
+    const command = {
+      type: CommandTypes.PERFORM_TRANSFORM,
+      key: "create-timestamp-seconds",
+    };
+
+    cmd$.next(command);
+
+    const source$ = getEditorChanges(cmd$).pipe(first());
+
+    const result = [];
+    for await (const value of eachValueFrom(source$)) {
+      result.push(value);
+    }
+
+    expect(view.hasFocus).toBeTruthy();
+    expect(view.state.doc.toString()).toBe("content.");
+
+    expect(execScriptSpy).toHaveBeenCalled();
+    expect(isAppendableScriptSpy).not.toHaveBeenCalled();
+
+    expect(focusSpy).toHaveBeenCalled();
+    expect(dispatchSpy).toHaveBeenCalled();
+  });
+});
