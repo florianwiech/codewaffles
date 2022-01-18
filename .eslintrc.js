3f389c9b78ae8af4d321844da7f636f924a13d37
Florian Wiech <florian.wiech@gmail.com>
2022-01-18 22:04:08 +0100

    chore: adapt eslint config setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,13 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2019,
+    project: ["./tsconfig.json", "./packages/**/tsconfig.json"],
     sourceType: "module",
   },
   plugins: ["import"],
-  extends: ["eslint:recommended", "prettier", "react-app", "react-app/jest"],
+  extends: ["eslint:recommended", "prettier", "react-app", "react-app/jest", "plugin:rxjs/recommended"],
   rules: {
     "import/order": "warn",
     "no-console": "warn",
   },
-  overrides: [
-    {
-      files: ["**/*.ts?(x)"],
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        ecmaVersion: 2019,
-        sourceType: "module",
-        project: "./tsconfig.json",
-        ecmaFeatures: {
-          jsx: true,
-        },
-        warnOnUnsupportedTypeScriptVersion: true,
-      },
-      extends: ["plugin:rxjs/recommended"],
-    },
-  ],
 };
