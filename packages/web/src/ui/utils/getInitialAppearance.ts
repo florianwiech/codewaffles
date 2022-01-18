9f39bf9ca289855c5adc2885254927fde37d2b3e
Florian Wiech <florian.wiech@gmail.com>
2022-01-18 20:18:59 +0100

    refactor: no empty block statement

--- packages/web/src/ui/utils/getInitialAppearance.ts
+++ packages/web/src/ui/utils/getInitialAppearance.ts
@@ -5,12 +5,11 @@ export const getInitialAppearance = (): AppearanceState => {
   try {
     const themeAppearance = window.localStorage.getItem(APPEARANCE_STORAGE);
 
-    if (
-      themeAppearance === AppearanceState.DARK ||
-      themeAppearance === AppearanceState.LIGHT
-    ) {
+    if (themeAppearance === AppearanceState.DARK || themeAppearance === AppearanceState.LIGHT) {
       return themeAppearance;
     }
-  } catch (e) {}
+  } catch (e) {
+    // ignore
+  }
   return AppearanceState.SYSTEM;
 };
