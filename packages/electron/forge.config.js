0b988135b7dd92fb1246191a6434dae2dec3a2a4
Florian Wiech <florian.wiech@gmail.com>
2022-01-20 18:57:33 +0100

    chore: update electron project structure

---
+++ packages/electron/forge.config.js
@@ -0,0 +1,43 @@
+const config = {
+  packagerConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "codewaffle",
+      },
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {},
+    },
+  ],
+  plugins: [
+    [
+      "@electron-forge/plugin-webpack",
+      {
+        mainConfig: "./webpack.main.config.js",
+        renderer: {
+          config: "./webpack.renderer.config.js",
+          entryPoints: [
+            {
+              html: "./static/index.html",
+              js: "./src/renderer/renderer.ts",
+              name: "main_window",
+            },
+          ],
+        },
+      },
+    ],
+  ],
+};
+
+module.exports = config;
