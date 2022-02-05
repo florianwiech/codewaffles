3c14ca42742c81b32c389490ab6ab672f1de44c0
Florian Wiech <florian.wiech@gmail.com>
2022-02-05 13:41:16 +0100

    refactor: rename transformers lib dir to scripts

--- packages/codewaffle-transformers/src/scriptList.ts
+++ packages/codewaffle-transformers/src/scriptList.ts
@@ -1,8 +1,8 @@
-import { createTimestampSeconds } from "./lib/createTimestampSeconds";
-import { createTimestampMillis } from "./lib/createTimestampMillis";
-import { decodeJwt } from "./lib/decodeJwt";
-import { countWords } from "./lib/countWords";
-import { countChars } from "./lib/countChars";
+import { createTimestampSeconds } from "./scripts/createTimestampSeconds";
+import { createTimestampMillis } from "./scripts/createTimestampMillis";
+import { decodeJwt } from "./scripts/decodeJwt";
+import { countWords } from "./scripts/countWords";
+import { countChars } from "./scripts/countChars";
 import { ScriptExtension } from "./types";
 
 export const scriptList: ScriptExtension[] = [
