c1d0f1dfa4a882dd9583bcb8e9f6dd9dca27d7de
Florian Wiech <florian.wiech@gmail.com>
2022-02-11 21:25:22 +0100

    chore: acceptFirstMouse in main electron window
    Only for macOS. UseCase: When clicking on the window to focus it should this mouse lick to focused be propagated to the web content?
    In native apps it seems to be the case, so I think this is a good idea.

--- apps/electron/src/main/windows/main-window.ts
+++ apps/electron/src/main/windows/main-window.ts
@@ -21,7 +21,7 @@ export function getMainWindowOptions(): Electron.BrowserWindowConstructorOptions
     minHeight: 600,
     minWidth: 320,
     titleBarStyle: process.platform === "darwin" ? "hidden" : undefined,
-    // acceptFirstMouse: true, // todo do we need this?
+    acceptFirstMouse: true,
     // backgroundColor: "#1d2427", // todo insert correct color based on appearance
     show: false,
     webPreferences: {
