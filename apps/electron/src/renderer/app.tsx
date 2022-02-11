abe1a993bd4832cfec32da14a08d9265542d7975
Florian Wiech <florian.wiech@gmail.com>
2022-02-11 19:49:41 +0100

    chore: add electron tailwind setup

--- apps/electron/src/renderer/app.tsx
+++ apps/electron/src/renderer/app.tsx
@@ -28,7 +28,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { MacOsTitleBar } from "./components/header";
-import "./app.css";
+import "./styles.css";
 
 ReactDOM.render(
   <>
