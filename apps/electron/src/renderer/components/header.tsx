18a889b5cdd44f6166e4c3c763072ef9b96a1239
Florian Wiech <florian.wiech@gmail.com>
2022-02-18 16:13:47 +0100

    feat(electron): style macOS title bar

--- apps/electron/src/renderer/components/header.tsx
+++ apps/electron/src/renderer/components/header.tsx
@@ -1,14 +1,21 @@
 import { FC } from "react";
-import "./header.css";
+import styled from "styled-components";
 
 type Props = { title: string; platform: string; onTitleBarClick: () => void };
+
+const StyledTitleBar = styled.div`
+  text-align: center;
+  background-color: ${({ theme }) => theme.canvas.default};
+  border-bottom: 1px solid ${({ theme }) => theme.border.default};
+  -webkit-app-region: drag;
+  height: 27px;
+  line-height: 27px;
+
+  user-select: none;
+`;
 
 export const MacOsTitleBar: FC<Props> = ({ title, platform, onTitleBarClick }) => {
   if (platform !== "darwin") return null;
 
-  return (
-    <div className="chrome drag" onDoubleClick={() => onTitleBarClick()}>
-      <small>{title}</small>
-    </div>
-  );
+  return <StyledTitleBar onDoubleClick={() => onTitleBarClick()}>{title}</StyledTitleBar>;
 };
