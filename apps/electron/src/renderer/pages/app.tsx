3b35d3597d0310d1811214a8dcf1b82c4f56fba0
Florian Wiech <florian.wiech@gmail.com>
2022-02-19 18:54:13 +0100

    refactor(electron): extract inline page templating to component

--- apps/electron/src/renderer/pages/app.tsx
+++ apps/electron/src/renderer/pages/app.tsx
@@ -1,40 +1,16 @@
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React, { FC } from "react";
+import ReactDOM from "react-dom";
 import { ElectronLayout } from "../components/ElectronLayout";
 import { MacTitleBar } from "../components/MacTitleBar";
 import { CodeMirror } from "../components/CodeMirror";
 
-ReactDOM.render(
-  <ElectronLayout>
-    <MacTitleBar title="CodeWaffle" platform={window.api.platform} onTitleBarClick={window.api.onTitleBarClick} />
-    <CodeMirror />
-  </ElectronLayout>,
-  document.getElementById("root"),
-);
+const App: FC = () => {
+  return (
+    <ElectronLayout>
+      <MacTitleBar title="CodeWaffle" platform={window.api.platform} onTitleBarClick={window.api.onTitleBarClick} />
+      <CodeMirror />
+    </ElectronLayout>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- apps/electron/src/renderer/pages/settings.tsx
+++ apps/electron/src/renderer/pages/settings.tsx
@@ -1,22 +1,25 @@
-import * as React from "react";
+import React, { FC } from "react";
 import * as ReactDOM from "react-dom";
 import { AppearanceState } from "@codewaffle/components";
 import { ElectronLayout } from "../components/ElectronLayout";
 import { MacTitleBar } from "../components/MacTitleBar";
 
-ReactDOM.render(
-  <ElectronLayout>
-    <MacTitleBar
-      title="CodeWaffle"
-      platform={window.settings.platform}
-      onTitleBarClick={window.settings.onTitleBarClick}
-    />
+const Settings: FC = () => {
+  return (
+    <ElectronLayout>
+      <MacTitleBar
+        title="CodeWaffle"
+        platform={window.settings.platform}
+        onTitleBarClick={window.settings.onTitleBarClick}
+      />
 
-    <h2>⚙️ Settings</h2>
+      <h2>⚙️ Settings</h2>
 
-    <button onClick={() => window.settings.changeAppearance(AppearanceState.SYSTEM)}>System</button>
-    <button onClick={() => window.settings.changeAppearance(AppearanceState.DARK)}>Dark</button>
-    <button onClick={() => window.settings.changeAppearance(AppearanceState.LIGHT)}>Light</button>
-  </ElectronLayout>,
-  document.getElementById("root"),
-);
+      <button onClick={() => window.settings.changeAppearance(AppearanceState.SYSTEM)}>System</button>
+      <button onClick={() => window.settings.changeAppearance(AppearanceState.DARK)}>Dark</button>
+      <button onClick={() => window.settings.changeAppearance(AppearanceState.LIGHT)}>Light</button>
+    </ElectronLayout>
+  );
+};
+
+ReactDOM.render(<Settings />, document.getElementById("root"));
