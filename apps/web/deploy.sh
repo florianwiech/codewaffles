ba3161026b06a467165d87519eab97f0085d09d9
Florian Wiech <florian.wiech@gmail.com>
2022-03-06 12:42:10 +0100

    feat(web): add deploy script

---
+++ apps/web/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+REGION='us-east-1'
+
+BUCKET=$(aws ssm get-parameter --name "/codewaffle/web/bucket/name" --query Parameter.Value --output text --region $REGION)
+
+echo "bucket: $BUCKET"
+
+aws s3 sync build s3://$BUCKET --delete --region $REGION
+
+DISTRIBUTION=$(aws ssm get-parameter --name "/codewaffle/web/distribution/id" --query Parameter.Value --output text --region $REGION)
+
+aws cloudfront create-invalidation --region $REGION --distribution-id $DISTRIBUTION --paths "/index.html"
