1d1690568acb4765e64aee3a8bfdae69f685aed9
Florian Wiech <florian.wiech@gmail.com>
2022-03-10 11:56:52 +0100

    chore(infrastructure): add region restriction note

--- apps/infrastructure/bin/infrastructure.ts
+++ apps/infrastructure/bin/infrastructure.ts
@@ -3,6 +3,8 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { WebStack } from "../lib/web-stack";
 
+// This template can only be deployed in the us-east-1 region.
+// This is because the ACM Certificate must be created in us-east-1
 const env: cdk.Environment = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
   region: "us-east-1",
