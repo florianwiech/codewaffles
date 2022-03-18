2878562aad2d0b3b678a45a2a3025894d7ca7c55
Florian Wiech <florian.wiech@gmail.com>
2022-03-18 20:11:23 +0100

    chore: independent jest setup

---
+++ apps/electron/jest.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
+export const config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  moduleNameMapper: {
+    "\\.svg$": "<rootDir>/__mocks__/svg.ts",
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "identity-obj-proxy",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+};
+
+export default config;
