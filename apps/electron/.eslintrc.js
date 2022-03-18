56edcb4f97780034d716f8abfff58a0f281d4991
Florian Wiech <florian.wiech@gmail.com>
2022-03-18 19:34:22 +0100

    chore: independent eslint setup

--- apps/electron/.eslintrc.js
+++ apps/electron/.eslintrc.js
@@ -1,5 +1,40 @@
-const base = require("@codewaffle/buildhelper/eslint/electron.cjs");
-
 module.exports = {
-  ...base,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2019,
+    project: ["./tsconfig.json"],
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  plugins: ["import"],
+  extends: [
+    //
+    "plugin:import/recommended",
+    "plugin:import/electron",
+    "plugin:import/typescript",
+    "react-app",
+    "react-app/jest",
+    "plugin:rxjs/recommended",
+    "prettier",
+  ],
+  rules: {
+    "import/order": "warn",
+    "no-console": "warn",
+  },
+  ignorePatterns: [
+    //
+    "**/*.js",
+    "**/*.cjs",
+    "**/*.json",
+    "node_modules",
+    ".webpack",
+    "static",
+    "coverage",
+    "dist",
+    "build",
+  ],
 };
