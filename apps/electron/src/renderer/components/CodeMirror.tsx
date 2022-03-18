2728a264e832246c13940db566a183ee2ba1af23
Florian Wiech <florian.wiech@gmail.com>
2022-03-18 20:37:40 +0100

    refactor: move useBehaviorSubject to hooks dir

--- apps/electron/src/renderer/components/CodeMirror.tsx
+++ apps/electron/src/renderer/components/CodeMirror.tsx
@@ -16,7 +16,7 @@ import {
 import { getEditorChanges } from "@codewaffle/domain";
 import { theme$ } from "../appearance";
 import { command$, editor$, notification$, view$ } from "../store";
-import { useBehaviorSubject } from "../shared/useBehaviorSubject";
+import { useBehaviorSubject } from "../shared/hooks/useBehaviorSubject";
 import { MAC_OS_TITLE_BAR_HEIGHT } from "./MacTitleBar";
 
 const macEditorHeight = css`
