6efc7abbb36d3c50702e28e6b5b08cf5eb18ca4f
Florian Wiech <florian.wiech@gmail.com>
2022-03-20 15:41:14 +0100

    chore: update native playwright config

--- playwright.native.config.ts
+++ playwright.native.config.ts
@@ -12,6 +12,12 @@ import { devices } from "@playwright/test";
  */
 const config: PlaywrightTestConfig = {
   testDir: "./e2e/native",
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: "html",
 };
 
 export default config;
