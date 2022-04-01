6910b08d8e48a3fe4e60ad9e0da9ebb79f2f7964
Florian Wiech <florian.wiech@gmail.com>
2022-04-01 17:26:57 +0200

    chore: add electron-builder

---
+++ tools/notarize.js
@@ -0,0 +1,32 @@
+const { notarize } = require("electron-notarize");
+
+/**
+ * @param {import("electron-builder").AfterPackContext} context
+ * @returns {(Promise<any> | any)}
+ */
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== "darwin") {
+    return;
+  }
+
+  if (!process.env.CI) {
+    console.log(`Not in CI, skipping notarization`);
+    return;
+  }
+
+  if (!process.env.APPLE_ID || !process.env.APPLE_ID_PASSWORD) {
+    console.warn("Should be notarizing, but environment variables APPLE_ID or APPLE_ID_PASSWORD are missing!");
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: "app.codewaffle",
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    ascProvider: "28563WWU78",
+  });
+};
