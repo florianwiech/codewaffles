d8c701161d031ca0d5a84bdfe9e7cf2baf611051
Florian Wiech <florian.wiech@gmail.com>
2022-08-27 16:23:46 +0200

    feat: restructure menu items

--- packages/main/menu/view-menu.ts
+++ packages/main/menu/view-menu.ts
@@ -1,42 +1,49 @@
 import type { MenuItemConstructorOptions } from "electron";
 import { BrowserWindow } from "electron";
+import { isDevMode } from "../utils/devmode";
+
+const reloadItems: MenuItemConstructorOptions[] = [
+  {
+    label: "Reload",
+    accelerator: "CmdOrCtrl+R",
+    click: function (item, focusedWindow) {
+      if (focusedWindow) focusedWindow.reload();
+    },
+  },
+  {
+    label: "Force Reload",
+    accelerator: "Shift+CmdOrCtrl+R",
+    click: function (item, focusedWindow) {
+      if (focusedWindow) focusedWindow.webContents.reloadIgnoringCache();
+    },
+  },
+  {
+    label: "Toggle Developer Tools",
+    accelerator: process.platform === "darwin" ? "Alt+Command+I" : "Ctrl+Shift+I",
+    click: function () {
+      const browserWindow = BrowserWindow.getFocusedWindow();
+
+      if (browserWindow && !browserWindow.isDestroyed()) {
+        browserWindow.webContents.toggleDevTools();
+      }
+    },
+  },
+  {
+    type: "separator",
+  },
+];
+
+const toggleFullScreenItem: MenuItemConstructorOptions = {
+  label: "Toggle Full Screen",
+  accelerator: process.platform === "darwin" ? "Ctrl+Command+F" : "F11",
+  click: function (item, focusedWindow) {
+    if (focusedWindow) focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+  },
+};
 
 export function getViewMenu(): MenuItemConstructorOptions {
   return {
     label: "View",
-    submenu: [
-      {
-        label: "Reload",
-        accelerator: "CmdOrCtrl+R",
-        click: function (item, focusedWindow) {
-          if (focusedWindow) focusedWindow.reload();
-        },
-      },
-      {
-        label: "Force Reload",
-        accelerator: "Shift+CmdOrCtrl+R",
-        click: function (item, focusedWindow) {
-          if (focusedWindow) focusedWindow.webContents.reloadIgnoringCache();
-        },
-      },
-      {
-        label: "Toggle Full Screen",
-        accelerator: process.platform === "darwin" ? "Ctrl+Command+F" : "F11",
-        click: function (item, focusedWindow) {
-          if (focusedWindow) focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-        },
-      },
-      {
-        label: "Toggle Developer Tools",
-        accelerator: process.platform === "darwin" ? "Alt+Command+I" : "Ctrl+Shift+I",
-        click: function () {
-          const browserWindow = BrowserWindow.getFocusedWindow();
-
-          if (browserWindow && !browserWindow.isDestroyed()) {
-            browserWindow.webContents.toggleDevTools();
-          }
-        },
-      },
-    ],
+    submenu: [...(isDevMode() ? reloadItems : []), toggleFullScreenItem],
   };
 }
